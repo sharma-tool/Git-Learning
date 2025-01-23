2e7eee614db72454fc180721c5a762c4d6178b7c
Vikash Mehta <vm98171@gmail.com>
2025-01-23 12:36:27 +0530

    upi done

--- first.js
+++ first.js
@@ -8,3 +8,6 @@ const footer = "footer added in our website";
 console.log(footer);
 const payment = "integrated the payment getway";
 console.log(payment);
+
+const upi = "integrated upi";
+console.log(upi);
